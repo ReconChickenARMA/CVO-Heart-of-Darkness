102f928bdfadebced65eb1e094c1df62c39604ed
OverlordZorn <dennisAzorn@gmail.com>
2025-10-15 04:38:39 +0200

    gear fnc

--- CVO-HOD-1.swu_public_rhode_map/CfgFunctions.hpp
+++ CVO-HOD-1.swu_public_rhode_map/CfgFunctions.hpp
@@ -6,6 +6,7 @@ class CfgFunctions {
         class init // Category
         {
             class init_babel { postInit = 1; };
+            class init_cigCrate { postInit = 1; };
             class init_diary { preInit = 1; };
             class init_events { preInit = 1; };
             class init_intel { postInit = 1; };
